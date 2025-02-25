30ae8055847657e9ef1615160f88d8adad4cb1ab
Julie <julie@bitcoin.com>
2025-02-25 10:03:38 +0900

    add components to lib

---
+++ webapp/src/components/Modal/index.js
@@ -0,0 +1,12 @@
+
+import Modal from './Modal.vue';
+import ModalBody from './ModalBody.vue';
+import ModalHeader from './ModalHeader.vue';
+import ModalProgress from './ModalProgress.vue';
+
+export {
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalProgress
+};
