5f114b887bde3436e41ebdce73a63ca4c167f110
Julie <julie@bitcoin.com>
2025-02-27 10:03:51 +0900

    add gas fee

--- webapp/src/helpers/wagmiContract.js
+++ webapp/src/helpers/wagmiContract.js
@@ -2,24 +2,25 @@ import {
     readContract,
     getBalance,
     writeContract,
-    waitForTransactionReceipt
   } from "@wagmi/core";
   import core from "../core";
   import globals from "../globals";
   import RouterContractABI from "../abi/router-contract.json";
 
-  export const buyTicketsWithEth = async (contractAddress, ticketCount, price) => {
+  export const buyTicketsWithEth = async (contractAddress, ticketCount, price, account) => {
+
     try {
       const hash = await writeContract(core.config, {
         address: globals.ROUTER_CONTRACT_ADDRESS,
         abi: RouterContractABI,
         functionName: 'buyTicketsWithETH',
         value: price,
+        gas: 75000n,
         args: [
           contractAddress,
           ticketCount,
           '0xedf6066a2b290c185783862c7f4776a2c8077ad1'
-        ]
+        ],
       })
       return hash;
     } catch (e) {
